bce8528093983100d2525310dc4bb5ccca3f61cb
Aliza Pilisuk <apilisuk@gmail.com>
2016-03-16 08:32:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var personName = prompt('Hey! I would love to get to know you a bit. What is your name?');
+alert('Wow ' + personName + ', what a fantastic name!');
+console.log('Hey! I would love to get to know you a bit. What is your name?');
+
+var personAge = prompt('How old are you?');
+alert('Aww, ' + personAge + ' is a good age. But remember, age is only a number.');
+console.log('How old are you?');
+
+var youngerAge = confirm('You would not want to be any younger, right...?');
+alert('I see you said ' + youngerAge + '. I will say it again, you are the perfect age :)');
+console.log('You would not want to be any younger, right...?');
+
+var personPets = confirm('Do you have any pets?');
+alert('I see you answered ' + personPets + '. Pets are sweet but I totally understand not wanting any.');
+console.log('Do you have any pets?');
+
+var dogsCats = confirm('Are dogs better than cats?');
+alert('That is ' + dogsCats + '! Dogs are better than cats.');
+console.log('Are dogs better than cats?');
+
+var amazingDay = confirm('It was great getting to know you. I hope you have an amazing day! Do you wish the same for me?');
+alert('I see you answered ' + amazingDay + ' to me having an amazing day. Well, I had my coffee so I think it is going to be a good day! Bye!');
+console.log('It was great getting to know you. I hope you have an amazing day! Do you wish the same for me?');
